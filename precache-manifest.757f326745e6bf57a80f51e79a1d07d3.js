ad9580047a07bbe55c7039a69ecd2bc4ee532a0f
Gilang Nuril Haqim <gilangn89@gmail.com>
2019-01-14 13:28:53 +0800

    Updates

---
+++ precache-manifest.757f326745e6bf57a80f51e79a1d07d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39cff1a79defb0fe09c",
+    "url": "/todolist-react/static/js/runtime~main.d39cff1a.js"
+  },
+  {
+    "revision": "2c13e991ae8a34514bec",
+    "url": "/todolist-react/static/js/main.2c13e991.chunk.js"
+  },
+  {
+    "revision": "7c38c5876a92c3641c85",
+    "url": "/todolist-react/static/js/1.7c38c587.chunk.js"
+  },
+  {
+    "revision": "2c13e991ae8a34514bec",
+    "url": "/todolist-react/static/css/main.099eb7bc.chunk.css"
+  },
+  {
+    "revision": "b1952f6997bcbc3a0e64bf66a8869dc5",
+    "url": "/todolist-react/index.html"
+  }
+];
